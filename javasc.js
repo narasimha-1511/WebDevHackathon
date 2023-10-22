da029fe0f2eff5b349c23cd7e64b91a14a07c2ae
narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>
2023-10-22 11:21:56 +0530

    Made Responsive + Edited css
    Signed-off-by: narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>

--- javasc.js
+++ javasc.js
@@ -1,76 +1,57 @@
-// document.getElementById("ad").innerHTML="dgfgfgfgfgf";
-
-console.log("Hello");
-
 const apiKey = "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";
-// var aa = (String)();
-// api key of chat gpt
-function newElement() {
-	if (document.getElementById("myInput").value != "") {
-		CALLDATA(document.getElementById("myInput").value);
-	} else {
-		document.getElementById("ad").innerHTML =
-			"Please Enter the Country Name";
-	}
+
+function StartFetch() {
+  if (document.getElementById("myInput").value != "") {
+    var data = document.getElementById("myInput").value;
+    if (data == "india") CALLDATA("India");
+    else CALLDATA(data);
+  } else {
+    document.getElementById("add").innerHTML = "Please Enter the Country Name";
+  }
 }
-var ndata = [
-	"population",
-	"capital",
-	"area",
-	"currencies",
-	"maps",
-	"timezones",
-	"languages",
-	"subregion",
-	"name",
-	"flag",
-	"region",
-];
 
 function CALLDATA(aaaa) {
-	let xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
 
-	xhr.open("GET", "https://restcountries.com/v3.1/name/" + aaaa, true);
-	// xhr.setRequestHeader('Authorization', ACCESS_TOKEN);
+  xhr.open("GET", "https://restcountries.com/v3.1/name/" + aaaa, true);
+  // xhr.setRequestHeader('Authorization', ACCESS_TOKEN);
 
-	xhr.onload = function () {
-		var data = this.responseText;
+  xhr.onload = function () {
+    var data = this.responseText;
 
-		const obj = JSON.parse(data);
+    const obj = JSON.parse(data);
 
-		// console.log(obj)
+    var datamini = "";
+    let a = 0;
+    if (aaaa == "india") a = 1;
 
-		var datamini = "";
-		let a = 0;
-		if (aaaa == "india") a = 1;
+    datamini += "Name: " + obj[a].name.common + "<br>";
+    datamini += "Flag : " + obj[a].flag + "<br>";
+    datamini += "Population:  " + obj[a].population + "<br>";
+    datamini += "Area:  " + obj[a].area + "<br>";
+    datamini += "Capital :" + obj[a].capital + "<br>";
 
-		datamini += "Name: " + obj[a].name.common + "<br>";
-		datamini += "Flag : " + obj[a].flag + "<br>";
-		datamini += "Population:  " + obj[a].population + "<br>";
-		datamini += "Area:  " + obj[a].area + "<br>";
-		datamini += "Capital :" + obj[a].capital + "<br>";
+    if (a === 1) {
+      datamini += "Currencies : " + obj[a].currencies.INR.name + "<br>";
+    }
 
-		if (a === 1) {
-			datamini += "Currencies : " + obj[a].currencies.INR.name + "<br>";
-		}
+    datamini += "Time Zone :" + obj[a].timezones + "<br>";
+    datamini += "Region :" + obj[a].region + "<br>";
+    datamini += "Sub Region: " + obj[a].subregion + "<br>";
 
-		datamini += "Time Zone :" + obj[a].timezones + "<br>";
-		datamini += "Region :" + obj[a].region + "<br>";
-		datamini += "Sub Region: " + obj[a].subregion + "<br>";
+    document.getElementsByTagName("a")[0].href = obj[a].maps.googleMaps;
+    document.getElementsByTagName("a")[1].href = obj[a].maps.openStreetMaps;
 
-		document.getElementsByTagName("a")[0].href = obj[a].maps.googleMaps;
-		document.getElementsByTagName("a")[1].href = obj[a].maps.openStreetMaps;
+    document.getElementById("add").innerHTML = datamini;
 
-		document.getElementById("add").innerHTML = datamini;
+    //Barcode
 
-		//Barcode
-
-		document.getElementById("imggoogle").src =
-			"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-			obj[a].maps.googleMaps;
-		document.getElementById("imgstreets").src =
-			"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-			obj[a].maps.openStreetMaps;
-	};
-	xhr.send();
+    document.getElementById("imggoogle").src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      obj[a].maps.googleMaps;
+    document.getElementById("imgstreets").src =
+      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+      obj[a].maps.openStreetMaps;
+  };
+  xhr.send();
 }
